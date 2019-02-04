19d78bccc2c531fb9f021eb688b641c6960a3a20
jamespagedev <jamespagedev@outlook.com>
2019-02-03 22:47:32 -0700

    postgresql-heroku test4

---
+++ data/seeds/001-users.js
@@ -0,0 +1,32 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users')
+    .truncate()
+    .then(function() {
+      // Inserts seed entries
+      return knex('users').insert([
+        {
+          username: 'jamespage',
+          display_name: 'RandomBlogger',
+          password: 'pass123',
+          email: 'jp@email.com',
+          img_url: 'https://i.imgur.com/mACq7e7.jpg'
+        },
+        {
+          username: 'catperson',
+          display_name: 'catperson',
+          password: 'cats1',
+          email: 'kitty@email.com',
+          img_url:
+            'http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg'
+        },
+        {
+          username: 'reader',
+          display_name: 'reader',
+          password: 'pass123',
+          email: '',
+          img_url: ''
+        }
+      ]);
+    });
+};
