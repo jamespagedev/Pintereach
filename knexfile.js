75981642244888c08c33b33d54ed566e26f5b024
jamespagedev <jamespagedev@outlook.com>
2019-02-03 22:23:09 -0700

    postgresql-heroku test3

--- knexfile.js
+++ knexfile.js
@@ -27,6 +27,12 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
   }
 };
