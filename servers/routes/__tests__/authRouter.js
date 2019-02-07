6d251cf2799b96c95769a1f62bdcadc92659cb4c
jamespagedev <jamespagedev@outlook.com>
2019-02-06 21:22:24 -0700

    passed failing test

--- servers/routes/__tests__/authRouter.js
+++ servers/routes/__tests__/authRouter.js
@@ -1,15 +1,17 @@
 const request = require('supertest');
-// const authRouter = require('../authRouter.js');
+const authRouter = require('../authRouter.js');
 const server = require('../../server.js');
 
-it('POST /auth/login', async () => {
-  const user = {
-    username: 'jamespage',
-    password: 'snes'
-  };
-  let response = await request(server)
-    .post('/auth/login')
-    .send(user);
+describe('authRouter.js', () => {
+  it('POST /auth/login', async () => {
+    const user = {
+      username: 'jamespage',
+      password: 'pass123'
+    };
+    let response = await request(server)
+      .post('/auth/login')
+      .send(user);
 
-  expect(response.status).toBe(201);
+    expect(response.status).toBe(201);
+  });
 });
